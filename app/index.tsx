dcc7feef771fb7c61153db5c39c64a404b8489bc
James Karl Apas <134823154+apaskarl@users.noreply.github.com>
2025-04-12 11:55:11 +0800

    Setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>asd app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
